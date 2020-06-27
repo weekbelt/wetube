84b7ee2f774b3c979be97ce78206955fe79f6e9c
김주혁 <vfrvfr4207@gmail.com>
2020-06-27 16:28:54 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get("/profile", handleProfile);
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
