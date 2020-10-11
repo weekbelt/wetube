b38c082bb29d80d81e230235e4ad6ff6d1fdb60c
weekbelt <vfrvfr4207@gmail.com>
2020-10-11 15:19:33 +0900

    refactor: init.js

--- init.js
+++ init.js
@@ -3,6 +3,6 @@ import app from "./app";
 const port = 4000;
 
 const handleListening = () =>
-  console.log(`Listening on: http://localhost:${port}`);
+  console.log(`✅ Listening on: http://localhost:${port}`);
 
 app.listen(port, handleListening);
