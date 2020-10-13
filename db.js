7bce807ad21c9ef3b710caea243cfd98166f95f1
weekbelt <vfrvfr4207@gmail.com>
2020-10-13 18:20:42 +0900

    Configuring Dot Env

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 mongoose.connect(
-  "mongodb://localhost:27017/we-tube", 
+  process.env.MONGO_URL, 
   {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   }
 );
 
@@ -14,53 +17,3 @@ const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log(`❌ Error on DB Connection: ${error}`);
 db.once("open", handleOpen);
 db.on("error", handleError);
-// export const videos = [
-//   {
-//     id: 324393,
-//     title: "Video awesome",
-//     description: "This is something I love",
-//     views: 24,
-//     videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-//     creator: {
-//       id: 121212,
-//       name: "Nicolas",
-//       email: "nico@las.com",
-//     },
-//   },
-//   {
-//     id: 1212121,
-//     title: "Video super",
-//     description: "This is something I love",
-//     views: 24,
-//     videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-//     creator: {
-//       id: 121212,
-//       name: "Nicolas",
-//       email: "nico@las.com",
-//     },
-//   },
-//   {
-//     id: 55555,
-//     title: "Video nice",
-//     description: "This is something I love",
-//     views: 24,
-//     videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-//     creator: {
-//       id: 121212,
-//       name: "Nicolas",
-//       email: "nico@las.com",
-//     },
-//   },
-//   {
-//     id: 11111,
-//     title: "Video perfect",
-//     description: "This is something I love",
-//     views: 24,
-//     videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-//     creator: {
-//       id: 121212,
-//       name: "Nicolas",
-//       email: "nico@las.com",
-//     },
-//   },
-// ];
--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config()
 
-const port = 4000;
+const port = process.env.PORT || 4000; 
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${port}`);
