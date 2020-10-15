b9f53006375ecdaaa8ff4bf7a0fca5d01cddb887
weekbelt <vfrvfr4207@gmail.com>
2020-10-15 22:50:20 +0900

    Styles with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,59 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const MODE = process.env.WEBPACK_ENV
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-    entry: ENTRY_FILE,
-    mode: MODE,
-    module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: ExtractCSS.extract([
+  entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
                     {
-                        loader: 'css-loader'
+                      overrideBrowserslist: "cover 99.5%",
+                      //   browsers: "cover 99.5%",
                     },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ])
-            }
-        ]
-    },
-    output: {
-        path: OUTPUT_DIR,
-        filename: "[name].[format]"
-    }
-}
+                  ],
+                ],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+    }),
+  ],
+};
 
 module.exports = config;
