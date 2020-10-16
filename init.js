0c2dee4bbec0cbeaa02122e44c7f431eb4cb0699
weekbelt <vfrvfr4207@gmail.com>
2020-10-16 17:20:10 +0900

    Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
-dotenv.config()
+
+dotenv.config();
+
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${port}`);
