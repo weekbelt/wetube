165b338c9ef7ebbd4e54cf812c86458fa877454d
weekbelt <vfrvfr4207@gmail.com>
2020-10-20 16:23:06 +0900

    Adding Creator to Video

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -16,6 +16,7 @@ userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
 userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
 userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
