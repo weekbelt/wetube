0f2ed8e4d1a46d90f3fdf2b918e92c7cf1c9ab33
weekbelt <vfrvfr4207@gmail.com>
2020-10-21 19:26:18 +0900

    Mute Unmute

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-    env: {
+  env: {
     browser: true,
     es2020: true,
     node: true,
-    },
-    extends: ["airbnb-base", "plugin:prettier/recommended"],
-    parserOptions: {
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
     ecmaVersion: 11,
     sourceType: "module",
-    },
-    rules: {
-        "prettier/prettier": "off",
-        "no-console": "off",
-        "spaced-comment": "off",
-        "no-else-return": "off"
-    }
+  },
+  rules: {
+    "prettier/prettier": "off",
+    "no-console": "off",
+    "spaced-comment": "off",
+    "no-else-return": "off",
+  },
+  env: {
+    browser: true,
+  },
 };
