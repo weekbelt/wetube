832416a28549dfb763d0aeb6084d439f8024793f
weekbelt <vfrvfr4207@gmail.com>
2020-10-22 16:13:24 +0900

    API Adding a Comment part One

--- routes.js
+++ routes.js
@@ -35,6 +35,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -82,6 +83,7 @@ const routes = {
   // facebookCallback: FB_CALLBACK,
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
